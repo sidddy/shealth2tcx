2d31673c8f72b1897d3b4d626dde934d4bb8ff3b
Sven <shenkel@gmail.com>
2017-01-31 11:19:19 +0100

    - Added Heart Rate information from live data files - fixed timestamps to be 24hrs - fix for empty location files - fix for calories and max speed\n

--- blob2tcx.php
+++ blob2tcx.php
@@ -7,7 +7,7 @@
 //
 // $name = $argv[1];
 function utcdate() {
-    return gmdate("Y-m-d\Th:i:s\Z");
+    return gmdate("Y-m-d\TH:i:s\Z");
 }
 
 function element($dom, $parent, $element_name) {
@@ -54,6 +54,22 @@ function blob2tcx($dirname, $mode, $meta)
         $json = json_decode(gzdecode($string), TRUE);
     } else
         $json = json_decode($string, TRUE);
+	
+	
+    if (!array_key_exists('live_data', $meta) || !strlen($meta['live_data'])) {
+        echo " no live data to decode\n";
+        return;
+    }
+    $name_live = $dirname.'/blobs/'.$mode.'/'.$meta['live_data'];
+    $string_live = file_get_contents($name_live);
+
+    if (!strlen($string_live))
+        return;
+
+    if (ord($string_live[0]) == 0x1f && ord($string_live[1]) == 0x8b) {
+        $json_live = json_decode(gzdecode($string_live), TRUE);
+    } else
+        $json_live = json_decode($string_live, TRUE);
 
     $dom_tcx = new DOMDocument('1.0', 'UTF-8');
     $dom_tcx->standalone = false;
@@ -76,31 +92,65 @@ function blob2tcx($dirname, $mode, $meta)
     case 1002: $excercise = 'Running'; break;
     case 11007: $excercise = 'Biking'; break;
     }
+    $start_time = DateTime::createFromFormat ( "Y-m-d H:i:s.u" , $meta['start_time']);
     $tcx_activity = element_with_attributes($dom_tcx, $tcx_activities, 'Activity', array('Sport' => $excercise));
-    element_with_text_node($dom_tcx, $tcx_activity, 'Id', gmdate("Y-m-d\Th:i:s\Z", $json[0]['start_time']/1000));
-    $lap = element_with_attributes($dom_tcx, $tcx_activity, 'Lap', array('StartTime' => gmdate("Y-m-d\Th:i:s\Z", $json[0]['start_time']/1000)));
+    element_with_text_node($dom_tcx, $tcx_activity, 'Id', gmdate("Y-m-d\TH:i:s\Z", $start_time->getTimestamp()));
+    $lap = element_with_attributes($dom_tcx, $tcx_activity, 'Lap', array('StartTime' => gmdate("Y-m-d\TH:i:s\Z", $start_time->getTimestamp())));
 
-    element_with_text_node($dom_tcx, $lap, 'TotalTimeSeconds', round((end($json)['start_time'] - $json[0]['start_time'])/1000));
+    element_with_text_node($dom_tcx, $lap, 'TotalTimeSeconds', ($meta['duration']/1000));
     element_with_text_node($dom_tcx, $lap, 'DistanceMeters', $meta['distance']);
-    element_with_text_node($dom_tcx, $lap, 'MaximumSpeed', '0');
-    element_with_text_node($dom_tcx, $lap, 'Calories', '0');
+    element_with_text_node($dom_tcx, $lap, 'MaximumSpeed', strlen($meta['max_speed']) ? $meta['max_speed']*3.6 : '0');
+    element_with_text_node($dom_tcx, $lap, 'Calories', strlen($meta['calorie']) ? $meta['calorie'] : '0');
     $lap_average_heart_rate_bpm = element_with_attributes($dom_tcx, $lap, 'AverageHeartRateBpm', array('xsi:type'=>'HeartRateInBeatsPerMinute_t'));
         element_with_text_node($dom_tcx, $lap_average_heart_rate_bpm, 'Value', strlen($meta['mean_heart_rate']) ? $meta['mean_heart_rate'] : 100);
     $lap_maximum_heart_rate_bpm = element_with_attributes($dom_tcx, $lap, 'MaximumHeartRateBpm', array('xsi:type'=>'HeartRateInBeatsPerMinute_t'));
-        element_with_text_node($dom_tcx, $lap_maximum_heart_rate_bpm, 'Value', strlen($meta['max_heart_rate']) ? $meta['mean_heart_rate'] : 200);
+        element_with_text_node($dom_tcx, $lap_maximum_heart_rate_bpm, 'Value', strlen($meta['max_heart_rate']) ? $meta['max_heart_rate'] : 200);
     element_with_text_node($dom_tcx, $lap, 'Intensity', 'Active');
     element_with_text_node($dom_tcx, $lap, 'TriggerMethod', 'Manual');
 
     $track = element($dom_tcx, $lap, 'Track');
-
-    foreach ($json as $point => $data) {
-        $trackpoint = element($dom_tcx, $track, 'Trackpoint');
-            element_with_text_node($dom_tcx, $trackpoint, 'Time', gmdate("Y-m-d\Th:i:s\Z", $data['start_time']/1000));
-            $trackpoint_pos = element($dom_tcx, $trackpoint, 'Position');
-                element_with_text_node($dom_tcx, $trackpoint_pos, 'LatitudeDegrees', $data['latitude']);
-                element_with_text_node($dom_tcx, $trackpoint_pos, 'LongitudeDegrees', $data['longitude']);
-                if(array_key_exists('altitude', $data))
-                    element_with_text_node($dom_tcx, $trackpoint_pos, 'AltitudeMeters', $data['altitude']);
+    $loc_cnt = 0;
+    $live_cnt = 0;
+    $heart = 0;
+    
+    while (($loc_cnt < sizeof($json)) || ($live_cnt < sizeof($json_live))) {
+	$data = null;
+	if ($loc_cnt < sizeof($json)) {
+		$data = $json[$loc_cnt];
+	}
+	
+	$data_live = null;
+	if ($live_cnt < sizeof($json_live)) {
+		$data_live = $json_live[$live_cnt];
+	}
+	
+	$live_time = 9999999999999;
+	$loc_time = 9999999999999;
+	if (!is_null($data)) {
+		$loc_time = $data['start_time'];
+	}
+	if (!is_null($data_live)) 
+		$live_time = $data_live['start_time'];
+		
+	if  ($loc_time  < $live_time) {
+		$trackpoint = element($dom_tcx, $track, 'Trackpoint');
+		element_with_text_node($dom_tcx, $trackpoint, 'Time', gmdate("Y-m-d\TH:i:s\Z", $data['start_time']/1000));
+		$trackpoint_pos = element($dom_tcx, $trackpoint, 'Position');
+		element_with_text_node($dom_tcx, $trackpoint_pos, 'LatitudeDegrees', $data['latitude']);
+		element_with_text_node($dom_tcx, $trackpoint_pos, 'LongitudeDegrees', $data['longitude']);
+		if(array_key_exists('altitude', $data))
+		    element_with_text_node($dom_tcx, $trackpoint_pos, 'AltitudeMeters', $data['altitude']);
+		if ($heart > 0) {
+			$heart_rate_bpm = element_with_attributes($dom_tcx, $trackpoint, 'HeartRateBpm', array('xsi:type'=>'HeartRateInBeatsPerMinute_t'));
+			element_with_text_node($dom_tcx, $heart_rate_bpm, 'Value', $heart);
+		}
+		$loc_cnt = $loc_cnt + 1;
+	} else {
+		if(array_key_exists('heart_rate', $data_live)) {
+			$heart = $data_live['heart_rate'];
+		}
+		$live_cnt = $live_cnt + 1;
+	}
     }
 
 //     header("Content-Type: text/xml");
